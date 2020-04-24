c988e51dd976295caa77c87ffd723a0195de478f
Keval <keval@thechubbypanda.net>
2020-04-24 12:10:07 +0100

    Proper use of families in EnemySystem
    Fixed error on player removal

--- src/main/java/net/thechubbypanda/larrysadventure/systems/EnemySystem.java
+++ src/main/java/net/thechubbypanda/larrysadventure/systems/EnemySystem.java
@@ -5,6 +5,7 @@ import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.ashley.utils.ImmutableArray;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.World;
@@ -16,6 +17,7 @@ public class EnemySystem extends IteratingSystem {
 
 	private final World world;
 	private final ComponentMapper<PhysicsComponent> epc = ComponentMapper.getFor(PhysicsComponent.class);
+	private ImmutableArray<Entity> players;
 	private Fixture last = null;
 
 	public EnemySystem(World world) {
@@ -27,14 +29,14 @@ public class EnemySystem extends IteratingSystem {
 	@Override
 	public void addedToEngine(Engine engine) {
 		super.addedToEngine(engine);
+		players = engine.getEntitiesFor(Family.all(PlayerComponent.class).get());
 	}
 
 	@Override
 	protected void processEntity(Entity entity, float deltaTime) {
 		// TODO: Potentially slow
-		Entity player;
-		if ((player = getEngine().getEntitiesFor(Family.all(PlayerComponent.class).get()).get(0)) != null) {
-			PhysicsComponent ppc = player.getComponent(PhysicsComponent.class);
+		for (Entity player : players) {
+			PhysicsComponent ppc = epc.get(player);
 			if (epc.has(entity)) {
 				PhysicsComponent pc = epc.get(entity);
 				last = null;
