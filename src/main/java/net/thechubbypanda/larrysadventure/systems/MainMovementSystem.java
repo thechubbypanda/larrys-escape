a22d2489a2bc986e1c47660178188cbb35b601fb
Keval <keval@thechubbypanda.net>
2020-05-05 14:26:33 +0100

    Variable name conformity

--- src/main/java/net/thechubbypanda/larrysadventure/systems/MainMovementSystem.java
+++ src/main/java/net/thechubbypanda/larrysadventure/systems/MainMovementSystem.java
@@ -13,25 +13,25 @@ import net.thechubbypanda.larrysadventure.components.SpriteComponent;
 
 public class MainMovementSystem extends IteratingSystem {
 
-	private final ComponentMapper<PhysicsComponent> physicsMapper = ComponentMapper.getFor(PhysicsComponent.class);
-	private final ComponentMapper<SpriteComponent> spriteMapper = ComponentMapper.getFor(SpriteComponent.class);
-	private final ComponentMapper<AnimationComponent> animationMapper = ComponentMapper.getFor(AnimationComponent.class);
+	private final ComponentMapper<PhysicsComponent> pcm = ComponentMapper.getFor(PhysicsComponent.class);
+	private final ComponentMapper<SpriteComponent> scm = ComponentMapper.getFor(SpriteComponent.class);
+	private final ComponentMapper<AnimationComponent> acm = ComponentMapper.getFor(AnimationComponent.class);
 
 	public MainMovementSystem() {
-		super(Family.all(PhysicsComponent.class).exclude(PlayerComponent.class).get());
+		super(Family.all(PhysicsComponent.class).one(SpriteComponent.class, AnimationComponent.class).exclude(PlayerComponent.class).get());
 	}
 
 	@Override
 	protected void processEntity(Entity entity, float deltaTime) {
-		Vector2 position = physicsMapper.get(entity).getPosition();
-		float rotation = physicsMapper.get(entity).getRotation();
-		if (spriteMapper.has(entity)) {
-			spriteMapper.get(entity).setPosition(position);
-			spriteMapper.get(entity).sprite.setRotation(rotation * MathUtils.radiansToDegrees);
+		Vector2 position = pcm.get(entity).getPosition();
+		float rotation = pcm.get(entity).getRotation();
+		if (scm.has(entity)) {
+			scm.get(entity).setPosition(position);
+			scm.get(entity).sprite.setRotation(rotation * MathUtils.radiansToDegrees);
 		}
-		if (animationMapper.has(entity)) {
-			animationMapper.get(entity).setPosition(position);
-			animationMapper.get(entity).setRotation(rotation);
+		if (acm.has(entity)) {
+			acm.get(entity).setPosition(position);
+			acm.get(entity).setRotation(rotation);
 		}
 	}
 }
