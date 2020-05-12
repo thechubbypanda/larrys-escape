82088808e2bb37b35d40ae538862a8bdbb41c049
Keval <keval@thechubbypanda.net>
2020-05-12 09:34:14 +0100

    Queue because for some reason Java and Gdx ones are shit

---
+++ src/main/java/net/thechubbypanda/larrysadventure/Queue.java
@@ -0,0 +1,24 @@
+package net.thechubbypanda.larrysadventure;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class Queue<T> extends Stack<T> {
+
+	@Override
+	public synchronized T peek() {
+		if (size() == 0)
+			throw new EmptyStackException();
+		return elementAt(0);
+	}
+
+	@Override
+	public synchronized T pop() {
+		T obj;
+
+		obj = peek();
+		removeElementAt(0);
+
+		return obj;
+	}
+}
