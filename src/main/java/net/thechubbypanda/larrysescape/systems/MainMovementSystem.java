21ae40268cdcaf8f29b7a43b28c9ee624923daab
Keval <keval@thechubbypanda.net>
2020-05-27 21:43:14 +0100

    Fix sprite rotations

--- src/main/java/net/thechubbypanda/larrysescape/systems/MainMovementSystem.java
+++ src/main/java/net/thechubbypanda/larrysescape/systems/MainMovementSystem.java
@@ -5,16 +5,14 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.gdx.math.Vector2;
-import net.thechubbypanda.larrysescape.components.AnimationComponent;
-import net.thechubbypanda.larrysescape.components.PlayerComponent;
-import net.thechubbypanda.larrysescape.components.SpriteComponent;
-import net.thechubbypanda.larrysescape.components.TransformComponent;
+import net.thechubbypanda.larrysescape.components.*;
 
 public class MainMovementSystem extends IteratingSystem {
 
 	private final ComponentMapper<TransformComponent> tcm = ComponentMapper.getFor(TransformComponent.class);
 	private final ComponentMapper<SpriteComponent> scm = ComponentMapper.getFor(SpriteComponent.class);
 	private final ComponentMapper<AnimationComponent> acm = ComponentMapper.getFor(AnimationComponent.class);
+	private final ComponentMapper<EnemyComponent> ecm = ComponentMapper.getFor(EnemyComponent.class);
 
 	public MainMovementSystem() {
 		super(Family.all(TransformComponent.class).one(SpriteComponent.class, AnimationComponent.class).exclude(PlayerComponent.class).get());
@@ -26,7 +24,11 @@ public class MainMovementSystem extends IteratingSystem {
 		float rotation = tcm.get(entity).getRotation();
 		if (scm.has(entity)) {
 			scm.get(entity).setPosition(position);
-			scm.get(entity).setRotation(rotation - 90);
+			if (ecm.has(entity)) {
+				scm.get(entity).setRotation(rotation - 90);
+			} else {
+				scm.get(entity).setRotation(rotation);
+			}
 		}
 		if (acm.has(entity)) {
 			acm.get(entity).setPosition(position);
